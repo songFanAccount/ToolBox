4923bdcb532da5942ee1a7bd5cf20b9458e10952
Daniel Song <z5260059@ad.unsw.edu.au>
2023-05-29 18:31:24 +1000

    Turned off autocapitalisation for MEP textfield

--- src/components/Maths/GeneralComponents.jsx
+++ src/components/Maths/GeneralComponents.jsx
@@ -5,7 +5,7 @@ export function MEPTextField({handleChange, expr}) {
         <TextField
             placeholder="e.g. ax^2 + bx + c"
             sx={{maxWidth: 500,}}
-            inputProps={{ maxLength: 150 }}
+            inputProps={{ maxLength: 150, autoCapitalize: 'none' }}
             onChange={(e) => handleChange(e)}
             value={expr}
         />
