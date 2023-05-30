d6f31b80f91dcf065bb814f3d53f728f57430431
Daniel Song <z5260059@ad.unsw.edu.au>
2023-05-30 17:39:00 +1000

    Abstracted out common themes of banners

--- src/components/UI/infoPage/InfoPageLayout.jsx
+++ src/components/UI/infoPage/InfoPageLayout.jsx
@@ -1,11 +1,11 @@
 import { Box } from "@mui/material";
-import { Outlet, useOutletContext } from "react-router-dom";
+import { Outlet, useLocation, useOutletContext } from "react-router-dom";
 import SideNavBar from "../SideNavBar";
 import React from "react";
 import { InfoPageTitle, PageTitle } from "../DefaultLayout";
 import { getLinear } from "../../../helpers/generalHelpers";
 
-function InfoPageBanner() {
+function InfoPageBanner({path}) {
     const [screenWidth, setScreenWidth] = React.useState(window.innerWidth)
     React.useEffect(() => {
         function handleResize() {
@@ -19,12 +19,45 @@ function InfoPageBanner() {
     }, [])
     let gifWidth = 0
     let dimX = 0
-    if(screenWidth > 400) {
+    if(screenWidth >= 400) {
         dimX = 1
     }
-    if(screenWidth > 1100) {
+    if(screenWidth >= 1100) {
         dimX = 2
         gifWidth = 384
+    }
+    let gif = null
+    let title = ""
+    let miniTitle = ""
+    let titleBoxSx = null
+    switch (path) {
+        case '/about':
+            title = "What is ToolBox?"
+            titleBoxSx = {
+                minWidth: `100vw`,
+                width: `100vw`,
+            }
+            break
+        case '/contact':
+            title = "Contact us!"
+            miniTitle = "Got questions?"
+            titleBoxSx = {
+                minWidth: `100vw`,
+                width: `100vw`,
+            }
+            break
+        case '/collab':
+            gif = <iframe title="thinking" src="https://giphy.com/embed/KD8cyaiEbZD0eKsA1J" width="384" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+            title = "Collaborate with us!"
+            miniTitle = "Got an idea?"
+            titleBoxSx = {
+                minWidth: `calc(100vw - 2 * ${gifWidth}px)`,
+                width: `calc(100vw - 2 * ${gifWidth}px)`,
+                overflowX: 'visible'
+            }
+            break
+        default:
+            throw new Error("InfoPageBanner: Unexpected path!")
     }
     const titleColor = dimX === 2 ? 'white' : '#05071f'
     const titleFS = getLinear(32, 70, 400, 1100, screenWidth)
@@ -45,7 +78,7 @@ function InfoPageBanner() {
                 boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px'
             }}
         >   
-            {dimX === 2 && 
+            {dimX === 2 && gif &&
                 <Box
                     sx={{
                         display: 'inline-block',
@@ -54,20 +87,18 @@ function InfoPageBanner() {
                         zIndex: 3,
                     }}
                 >
-                    <iframe title="thinking" src="https://giphy.com/embed/KD8cyaiEbZD0eKsA1J" width="384" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+                    {gif}
                 </Box>
             }
             <Box
                 sx={{
+                    ...titleBoxSx,
                     mt: 25,
-                    minWidth: `calc(100vw - 2 * ${gifWidth}px)`,
-                    width: `calc(100vw - 2 * ${gifWidth}px)`,
-                    overflowX: 'visible'
                 }}
             >
                 <Box sx={{width: 'fit-content', minWidth: 'fit-content', display:'flex', flexDirection: 'column', alignItems: 'start', mx: 'auto', whiteSpace: dimX === 0 ? 'normal' : 'nowrap'}}>
-                    <PageTitle title="Got an idea?" color={titleColor} fs={miniTitleFS} underline="dashed" mb={3}/>
-                    <InfoPageTitle title="Collaborate with us" color={titleColor} fs={titleFS}/>  
+                    {miniTitle !== "" && <PageTitle title={miniTitle} color={titleColor} fs={miniTitleFS} underline="dashed" mb={3}/>}
+                    <InfoPageTitle title={title} color={titleColor} fs={titleFS}/>  
                 </Box>
             </Box>
         </Box>
@@ -77,6 +108,7 @@ export default function InfoPageLayout() {
     const context = useOutletContext()
     const dimX = context.dimX
     const gap = dimX > 0 ? 160 : 20
+    const path = useLocation().pathname
     return (
         <Box>
             <Box
@@ -88,7 +120,7 @@ export default function InfoPageLayout() {
             >
                 {dimX > 0 && <SideNavBar inHeader={false}/>}
             </Box>
-            <InfoPageBanner/>
+            <InfoPageBanner path={path}/>
             <Box 
                 sx={{
                     position: 'relative',
