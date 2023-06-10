2845c0dbf56a145cc2ad2c57d7685ca1537ff715
harryJang <yunseokbb@gmail.com>
2023-06-10 21:45:20 +0900

    Instead of using displayName everywhere, made it use category object instead. Added some comments.

--- src/components/UI/categoryPage/CategoryPageLayout.jsx
+++ src/components/UI/categoryPage/CategoryPageLayout.jsx
@@ -2,50 +2,52 @@ import React, { useState } from "react"
 import { Box } from "@mui/material"
 
 import CategoryButton from "./CategoryButton"
+import CategoryDescription from "./CategoryDescription"
 import { tools } from "../../../data"
 
 export default function CategoryPageLayout({category}) {
-    const categories = generateCategoryButtonObjectList(category ? category.displayName : null)
-    const [chosenCategory, setChosenCategory] = useState(categories)
+    const categoriesButtonObject = generateCategoryButtonObjectList(category)
+    const [categoryButtons, setCategoryButtons] = useState(categoriesButtonObject)
+    const [selectedCategory, setSelectedCategory] = useState(category)
 
-    function handleClick(categoryName) {
-        setChosenCategory(prevChosenCategory => {
-            return prevChosenCategory.map((category) => {
-                if ((category.id === categoryName) || (category.id !== categoryName && category.selected)) {
-                    return {id: category.id, selected: !category.selected}
+    function handleClick(category) {
+        // it possibly changes one or two button/buttons and leave others as it is.
+        // case 1. if user select new category, it sets previously selected button to be unselected and new button to be selected.
+        // case 2. if user undo the button selected, it simply unselect that button (no button is selected at this stage).
+        // state selectedCategory will be updated to newly selected button or to null in case 2 .
+        setCategoryButtons(prevcategoryButtons => {
+            return prevcategoryButtons.map((Button) => {
+                if ((Button.id === category) || (Button.id !== category && Button.selected)) {
+                    if (Button.id === category) setSelectedCategory(!Button.selected ? Button.id : null)
+                    return {id: Button.id, selected: !Button.selected}
                 }
                 else {
-                    return category
+                    return Button
                 }
             })
         })
     }
 
-    const categoryButtons = chosenCategory.map( categoryButton => <CategoryButton key={categoryButton.id} categoryName={categoryButton.id} selected={categoryButton.selected} handleClick={handleClick}/>)
+    const categoryButtonsComponent = categoryButtons.map( categoryButton => <CategoryButton key={categoryButton.id.displayName} category={categoryButton.id} selected={categoryButton.selected} handleClick={handleClick}/>)
     return (
         <Box>
-            <Box>This page is for {category ? category.displayName : "all categories"}</Box>
             <Box 
                 sx={{display: "flex"}}
             >
-                {categoryButtons}
+                {categoryButtonsComponent}
             </Box>
+            {selectedCategory && <CategoryDescription category={selectedCategory}/>}
         </Box>
     )
 }
 
 function generateCategoryButtonObjectList(categoryName) {
-    const categories = Object.values(tools.subCategories).map(tool => tool.displayName)
+    // This function will create a list of objects of categories.
+    // Object looks like this - {id: display name of the category, selected: boolean}
+    const categories = [tools, ...Object.values(tools.subCategories)]
     let categoriesObjectList = []
-    if (categoryName) {
-        for (let i = 0; i < categories.length; i++) {
-            categoriesObjectList.push({id: categories[i], selected: categoryName === categories[i] ? true : false})
-        }
+    for (let i = 0; i < categories.length; i++) {
+        categoriesObjectList.push({id: categories[i], selected: categoryName === categories[i] ? true : false})
     }
-    else {
-        for (let i = 0; i < categories.length; i++) {
-            categoriesObjectList.push({id: categories[i], selected: false})
-        }
-    }
-    return [{id:"All Categories", selected: !categoryName ? true : false}, ...categoriesObjectList]
+    return categoriesObjectList
 }
