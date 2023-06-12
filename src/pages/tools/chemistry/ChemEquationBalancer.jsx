78f9d983c35be47a32ad38d533710b082d9a2a02
Daniel Song <z5260059@ad.unsw.edu.au>
2023-06-12 16:25:56 +1000

    Element count basic display done

--- src/pages/tools/chemistry/ChemEquationBalancer.jsx
+++ src/pages/tools/chemistry/ChemEquationBalancer.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { CEPTextField } from '../../../components/GeneralComponents'
 import { PageParagraph, SectionBox } from '../../../components/UI/DefaultLayout'
-import { Box, Typography } from '@mui/material'
+import { Box, Stack, Typography } from '@mui/material'
 import { useRef } from 'react'
 import { getChemEqnInfo } from '../../../helpers/chemHelpers'
 import { DisplayError } from '../../../components/Compsci/DataStructures'
@@ -15,7 +15,7 @@ export default function ChemEquationBalancer() {
     }, [expr])
     const EquationDisplay = () => {
         if(chemObj.current?.success) {
-            return <Typography>{`$\\ce{${chemObj.current.latex}}$`}</Typography>
+            return <Typography sx={{overflowX: 'scroll', height: 40}}>{`$\\ce{${chemObj.current.latex}}$`}</Typography>
         } else {
             const errorMsg = chemObj.current ? chemObj.current.errorMsg : "Empty input!"
             return <DisplayError errorMsg={errorMsg}/>
@@ -24,6 +24,55 @@ export default function ChemEquationBalancer() {
     function handleChange(value) {
         setExpr(value)
         chemObj.current = getChemEqnInfo(value)
+    }
+    function DisplayElementCount({elementCount}) {
+        return (
+            <Stack
+                direction="column"
+            >
+                {Object.entries(elementCount).map((value) => (
+                    <PageParagraph text={`${value[0]}: ${value[1]}`}/>
+                ))}
+            </Stack>
+        )
+    }
+    function Reactants() {
+        if(chemObj.current?.success) {
+            return (
+                <Box>
+                    <PageParagraph text="Reactants:"/>
+                    <Stack
+                        direction="row"
+                        columnGap={3}
+                    >
+                        {chemObj.current.reactants.map((reactant) => <DisplayElementCount elementCount={reactant.elementCount}/>)}
+                    </Stack>
+                </Box>
+            )
+        } else {
+            return (
+                <DisplayError errorMsg="Invalid reactants!"/>
+            )
+        }
+    }
+    function Products() {
+        if(chemObj.current?.success) {
+            return (
+                <Box>
+                    <PageParagraph text="Products:"/>
+                    <Stack
+                        direction="row"
+                        columnGap={3}
+                    >
+                        {chemObj.current.products.map((product) => <DisplayElementCount elementCount={product.elementCount}/>)}
+                    </Stack>
+                </Box>
+            )
+        } else {
+            return (
+                <DisplayError errorMsg="Invalid products!"/>
+            )
+        }
     }
     return (
         <Box>
@@ -36,6 +85,13 @@ export default function ChemEquationBalancer() {
                 <PageParagraph text="Enter an expression to begin:"/>
                 <CEPTextField onChange={handleChange} expr={expr}/>
                 <EquationDisplay/>
+                <Stack 
+                    direction="row"
+                    columnGap={3}
+                >
+                    <Reactants/>
+                    <Products/>
+                </Stack>
                 <SectionBox title="Step 1:" mb={0}>
                     <PageParagraph text={`Step 1`}/>
                 </SectionBox>
