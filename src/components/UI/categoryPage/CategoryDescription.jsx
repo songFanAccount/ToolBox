c9b2f34615bb2b55e4fc175aa0e25fd208f622fb
harryJang <yunseokbb@gmail.com>
2023-06-13 19:07:41 +0900

    made switch statement a function.

--- src/components/UI/categoryPage/CategoryDescription.jsx
+++ src/components/UI/categoryPage/CategoryDescription.jsx
@@ -2,32 +2,7 @@ import React from "react"
 import { Card } from "@mui/material"
 
 export default function CategoryDescription({category}) {
-    let categoryDesc
-    switch (category.displayName) {
-        case "Maths":
-            categoryDesc = <p>This is <strong>Maths</strong> Category</p>
-            break
-        case "Chemistry":
-            categoryDesc = <p>This is <strong>Chemistry</strong> Category</p>
-            break
-        case "Physics":
-            categoryDesc = <p>This is <strong>Physics</strong> Category</p>
-            break
-        case "Engineering":
-            categoryDesc = <p>This is <strong>Engineering</strong> Category</p>
-            break
-        case "IRL Games":
-            categoryDesc = <p>This is <strong>IRL Games</strong> Category</p>
-            break
-        case "Video Games":
-            categoryDesc = <p>This is <strong>Video Games</strong> Category</p>
-            break
-        case "Computer Science":
-            categoryDesc = <p>This is <strong>Computer Science</strong> Category</p>
-            break
-        default:
-            categoryDesc = <p>This is <strong>All Category</strong></p>
-    }
+    const categoryDesc = getDescription(category.displayName)
     return (
         <Card 
             variant="outlined"
@@ -39,4 +14,25 @@ export default function CategoryDescription({category}) {
             {categoryDesc}
         </Card>
     )
+}
+
+function getDescription(categoryName) {
+    switch (categoryName) {
+        case "Maths":
+            return <p>This is <strong>Maths</strong> Category</p>
+        case "Chemistry":
+            return <p>This is <strong>Chemistry</strong> Category</p>
+        case "Physics":
+            return <p>This is <strong>Physics</strong> Category</p>
+        case "Engineering":
+            return <p>This is <strong>Engineering</strong> Category</p>
+        case "IRL Games":
+            return <p>This is <strong>IRL Games</strong> Category</p>
+        case "Video Games":
+            return <p>This is <strong>Video Games</strong> Category</p>
+        case "Computer Science":
+            return <p>This is <strong>Computer Science</strong> Category</p>
+        default: // All categories
+            return <p>This is <strong>All Category</strong></p>
+    }
 }
