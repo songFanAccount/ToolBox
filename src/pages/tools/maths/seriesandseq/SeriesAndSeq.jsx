9afc04aeb8f52d5fb611e35e4c26119e845028f8
Daniel Song <z5260059@ad.unsw.edu.au>
2023-06-14 12:16:10 +1000

    SnS stub

--- src/pages/tools/maths/seriesandseq/SeriesAndSeq.jsx
+++ src/pages/tools/maths/seriesandseq/SeriesAndSeq.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { PageParagraph, SectionBox } from '../../../../components/UI/DefaultLayout'
+import { Box } from '@mui/material'
+import { MEPTextField } from '../../../../components/GeneralComponents'
 
 export default function SeriesAndSequences() {
+    const [expr, setExpr] = useState('')
+    function handleChange(value) {
+        setExpr(value)
+    }
     return (
-        <div>SeriesAndSequences</div>
+        <Box>
+            <SectionBox noBorder={true}>
+                <PageParagraph text={
+                    `Description`
+                }/>
+            </SectionBox>
+            <SectionBox noBorder>
+                <PageParagraph text="Please provide all the information you know about the series/sequence:"/>
+                <MEPTextField onChange={handleChange} expr={expr}/>
+                <SectionBox title="Step 1:" mb={0}>
+                    <PageParagraph text={`Step 1`}/>
+                </SectionBox>
+                <SectionBox title="Step 2:" mb={0}>
+                    <PageParagraph text={`Step 2`}/>
+                </SectionBox>
+                <SectionBox title="Step 3:" mb={0}>
+                    <PageParagraph text={`Step 3`}/>
+                </SectionBox>
+                <SectionBox title="Step 4:" mb={0}>
+                    <PageParagraph text={`Step 4`}/>
+                </SectionBox>
+            </SectionBox>
+        </Box>
     )
 }
