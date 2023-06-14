a21dd11fb2c190c55b67834d8551ec043c75ced3
Daniel Song <z5260059@ad.unsw.edu.au>
2023-06-14 14:08:33 +1000

    Basic UI components

--- src/pages/Compiler.jsx
+++ src/pages/Compiler.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TBTextField } from '../components/UI/Form'
+import { Box } from '@mui/material'
+import { TBButton } from '../components/UI/DefaultLayout'
 
 export default function Compiler() {
+    const [code, setCode] = useState('')
+    function compile() {
+        console.log('compile')
+    }
     return (
-        <div>Compiler</div>
+        <Box>
+            <TBTextField
+                onChange={setCode}
+                value={code}
+                rows={20}
+                variant='outlined'
+                placeholder='Insert code here...'
+                width={600}
+            />
+            <TBButton 
+                buttonText="Compile"
+                onClick={compile}
+            />
+        </Box>
     )
 }
