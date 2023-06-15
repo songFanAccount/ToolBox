6e687128e8bea28d89e16a50a3f390eea7813263
harryJang <yunseokbb@gmail.com>
2023-06-16 03:23:34 +0900

    removed Learn More button and made whole card a button. made getDescription function. made description scrollable when overflow.

--- src/components/UI/categoryPage/CategoryToolCard.jsx
+++ src/components/UI/categoryPage/CategoryToolCard.jsx
@@ -1,43 +1,61 @@
 import React from "react"
-import { Card, CardContent, Typography, CardActions, Button } from "@mui/material"
+import { Box, Card, CardContent, Typography } from "@mui/material"
 
 import { ToolLink } from "../DefaultLayout"
 
 export default function CategoryToolCard({catWithTool}) {
-    const link = <ToolLink 
-                    name={catWithTool.tool.displayName.toLowerCase()}
-                    linkText={<Button size="small" sx={{fontFamily: 'Verdana', color: "#218f1f"}}>Learn More</Button>} 
-                />
+    const desc = getDescription(catWithTool.tool.displayName)
     return (
-        <Card 
-            sx={{
-                  m: 0.5,
-                  height: "200px",
-                  position: "relative"
-                }} 
-            variant="outlined"
-        >
-            <CardContent>
-                <Typography 
-                    sx={{
-                        fontSize: 14,
-                        fontFamily: 'Montserrat'
-                        }} 
-                    color="text.secondary"
-                    gutterBottom
-                >
-                    {catWithTool.cat}
-                </Typography>
-                <Typography variant="h5" component="div" sx={{fontFamily: 'Verdana',}}>
-                    {catWithTool.tool.displayName}
-                </Typography>
-                <Typography variant="body2" sx={{fontFamily: 'Verdana',}}>
-                    brief <strong>explanation</strong> goes here
-                </Typography>
-            </CardContent>
-            <CardActions sx={{position: 'absolute', bottom: 0}}>
-                {link}
-            </CardActions>
-        </Card>
+        <ToolLink name ={catWithTool.tool.displayName.toLowerCase()}
+        linkText={
+            <Card 
+                sx={{
+                    m: 0.5,
+                    height: 200
+                    }} 
+                variant="outlined"
+            >
+                <CardContent>
+                    <Typography 
+                        sx={{
+                            fontSize: 14,
+                            fontFamily: 'Montserrat'
+                            }} 
+                        color="text.secondary"
+                        gutterBottom
+                    >
+                        {catWithTool.cat}
+                    </Typography>
+                    <Typography variant="h5" component="div" sx={{fontFamily: 'Verdana', mb: 0.5}}>
+                        {catWithTool.tool.displayName}
+                    </Typography>
+                    <Box component="div" sx={{maxHeight: 80, overflow: 'auto'}}>
+                        <Typography variant="body2" sx={{fontFamily: 'Verdana', ml: 0.5, pb: 1}}>
+                            {desc}
+                        </Typography>
+                    </Box>
+                </CardContent>
+            </Card>
+        }/>
     )
+}
+
+function getDescription(toolDisplayName) {
+    switch(toolDisplayName) {
+        case "LaTeX Converter":
+            return "This is description for LaTeX Converter \
+                    This is description for LaTeX Converter \
+                    This is description for LaTeX Converter \
+                    This is description for LaTeX Converter \
+                    This is description for LaTeX Converter \
+                    This is description for LaTeX Converter"
+        case "Stationary points":
+            return "This is description for stationary points"
+        case "Chemistry Equation Balancer":
+            return "This is description for CEB"
+        case "Maths expression parser":
+            return "This is description for MEP"
+        default:
+            return "-"
+    }
 }
