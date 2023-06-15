f0c016b6d713029ce3abca7a555f4897a892c6ec
harryJang <yunseokbb@gmail.com>
2023-06-16 03:33:50 +0900

    added tooltip to indicate card can be clicked to check out more

--- src/components/UI/categoryPage/CategoryToolCard.jsx
+++ src/components/UI/categoryPage/CategoryToolCard.jsx
@@ -1,41 +1,43 @@
 import React from "react"
-import { Box, Card, CardContent, Typography } from "@mui/material"
+import { Box, Card, CardContent, Tooltip, Typography } from "@mui/material"
 
 import { ToolLink } from "../DefaultLayout"
 
 export default function CategoryToolCard({catWithTool}) {
     const desc = getDescription(catWithTool.tool.displayName)
     return (
-        <ToolLink name ={catWithTool.tool.displayName.toLowerCase()}
+        <ToolLink name={catWithTool.tool.displayName.toLowerCase()}
         linkText={
-            <Card 
-                sx={{
-                    m: 0.5,
-                    height: 200
-                    }} 
-                variant="outlined"
-            >
-                <CardContent>
-                    <Typography 
-                        sx={{
-                            fontSize: 14,
-                            fontFamily: 'Montserrat'
-                            }} 
-                        color="text.secondary"
-                        gutterBottom
-                    >
-                        {catWithTool.cat}
-                    </Typography>
-                    <Typography variant="h5" component="div" sx={{fontFamily: 'Verdana', mb: 0.5}}>
-                        {catWithTool.tool.displayName}
-                    </Typography>
-                    <Box component="div" sx={{maxHeight: 80, overflow: 'auto'}}>
-                        <Typography variant="body2" sx={{fontFamily: 'Verdana', ml: 0.5, pb: 1}}>
-                            {desc}
+            <Tooltip title="Click to check out!" arrow>
+                <Card 
+                    sx={{
+                        m: 0.5,
+                        height: 200
+                        }}
+                    variant="outlined"
+                >
+                    <CardContent>
+                        <Typography 
+                            sx={{
+                                fontSize: 14,
+                                fontFamily: 'Montserrat'
+                                }} 
+                            color="text.secondary"
+                            gutterBottom
+                        >
+                            {catWithTool.cat}
                         </Typography>
-                    </Box>
-                </CardContent>
-            </Card>
+                        <Typography variant="h5" component="div" sx={{fontFamily: 'Verdana', mb: 0.5}}>
+                            {catWithTool.tool.displayName}
+                        </Typography>
+                        <Box component="div" sx={{maxHeight: 80, overflow: 'auto'}}>
+                            <Typography variant="body2" sx={{fontFamily: 'Verdana', ml: 0.5, pb: 1}}>
+                                {desc}
+                            </Typography>
+                        </Box>
+                    </CardContent>
+                </Card>
+            </Tooltip>
         }/>
     )
 }
