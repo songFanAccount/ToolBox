8d9bdd6130111f3f1493b463f56445e208c1ae6d
harryJang <yunseokbb@gmail.com>
2023-06-17 02:51:34 +0900

    added parameter query to keep track on which category is chosen.

--- src/components/UI/categoryPage/CategoryButton.jsx
+++ src/components/UI/categoryPage/CategoryButton.jsx
@@ -1,28 +1,31 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import { Button, Typography } from "@mui/material"
 
 export default function CategoryButton({category, selected, handleClick}) {
 
     return (
-        <Button
-            variant={selected ? "contained" : "outlined"}
-            color={selected ? "success" : "primary"}
-            onClick={() => handleClick(category)}
-            disableRipple
-            sx={{
-                margin: "5px",
-                minWidth: "fit-content",
-                whiteSpace: "nowrap"
-            }}
-        >
-            <Typography
+        <Link to={`/tools/?category=${category.displayName}`}>
+            <Button
+                variant={selected ? "contained" : "outlined"}
+                color={selected ? "success" : "primary"}
+                onClick={() => handleClick(category)}
+                disableRipple
                 sx={{
-                    fontFamily: 'Montserrat',
-                    fontSize: 13
+                    margin: "5px",
+                    minWidth: "fit-content",
+                    whiteSpace: "nowrap"
                 }}
             >
-                {category.displayName}
-            </Typography>
-        </Button>
+                <Typography
+                    sx={{
+                        fontFamily: 'Montserrat',
+                        fontSize: 13
+                    }}
+                >
+                    {category.displayName}
+                </Typography>
+            </Button>
+        </Link>
     )
 }
