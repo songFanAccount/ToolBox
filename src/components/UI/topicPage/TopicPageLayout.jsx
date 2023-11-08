05d413d8e91eeebd954800def1ae468acb27f280
Daniel <daniel.song929@gmail.com>
2023-11-09 02:34:22 +1100

    Added navbar button to topicpage

--- src/components/UI/topicPage/TopicPageLayout.jsx
+++ src/components/UI/topicPage/TopicPageLayout.jsx
@@ -1,11 +1,46 @@
-import { Box } from "@mui/material";
+import React from 'react';
 import { PageTitle } from "../DefaultLayout";
+import { Helmet } from "react-helmet";
+import SideNavBar from "../SideNavBar";
+import { Box } from "@mui/material";
 
 export default function TopicPageLayout({topic}) {
     const pageTitle = topic;
+    const [screenWidth, setScreenWidth] = React.useState(window.innerWidth)
+    React.useEffect(() => {
+        function handleResize() {
+            setScreenWidth(window.innerWidth)
+        }
+        window.addEventListener("resize", handleResize)
+        return _ => {
+            window.removeEventListener("resize", handleResize)
+        }
+    // eslint-disable-next-line
+    }, [])
+    const hasNavbar = screenWidth >= 900
+    const toolButtonWidth = 160
     return (
-        <Box>
-            <PageTitle title={pageTitle}/>
-        </Box>
+        <>
+            <Helmet>
+                <title>{pageTitle}</title>
+            </Helmet>
+            <Box
+                sx={{
+                    display: 'flex'
+                }}
+            >
+                {hasNavbar &&
+                    <Box
+                        sx={{
+                            width: toolButtonWidth,
+                            zIndex: 3,
+                        }}
+                    >
+                        <SideNavBar inHeader={false}/>
+                    </Box>
+                }
+                <PageTitle title={pageTitle} fs={24}/>
+            </Box>
+        </>
     )
 }
