e9c9c48862296138395256213750f5472f22df27
Daniel <daniel.song929@gmail.com>
2023-11-15 15:19:26 +1100

    Added add/remove item buttons

--- src/pages/tools/compsci/AI/resourceAlloc/RASimulator.jsx
+++ src/pages/tools/compsci/AI/resourceAlloc/RASimulator.jsx
@@ -3,10 +3,13 @@ import React from 'react'
 import { CBIconButton, ControlBoardBox } from '../../../../../components/UI/Animation'
 import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
 import PersonRemoveAlt1Icon from '@mui/icons-material/PersonRemoveAlt1';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 export default function RASimulator() {
     const sqrWidth = 40
-    const [agents, setAgents] = React.useState(0)
+    const [numAgents, setNumAgents] = React.useState(0)
+    const [numItems, setNumItems] = React.useState(0)
     const TopRow = () => (
         <Stack
             direction="row"
@@ -53,18 +56,28 @@ export default function RASimulator() {
         )
     }
     function addAgent() {
-        if (agents === 10) return
-        setAgents(agents + 1)
+        if (numAgents === 10) return
+        setNumAgents(numAgents + 1)
     }
     function removeAgent() {
-        if (agents === 0) return
-        setAgents(agents - 1)
+        if (numAgents === 0) return
+        setNumAgents(numAgents - 1)
+    }
+    function addItem() {
+        if (numItems === 10) return
+        setNumItems(numItems + 1)
+    }
+    function removeItem() {
+        if (numItems === 0) return
+        setNumItems(numItems - 1)
     }
     const ControlBoard = () => {
         return (
             <ControlBoardBox label="Control board">
                 <CBIconButton tooltip="Add agent (up to 10)" onClick={addAgent} icon={<PersonAddAlt1Icon/>}/>
                 <CBIconButton tooltip="Remove agent (bottom row)" onClick={removeAgent} icon={<PersonRemoveAlt1Icon/>}/>
+                <CBIconButton tooltip="Add item (up to 10)" onClick={addItem} icon={<AddIcon/>}/>
+                <CBIconButton tooltip="Remove item (right-most column)" onClick={removeItem} icon={<RemoveIcon/>}/>
             </ControlBoardBox>
         )
     }
