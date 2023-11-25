7fd6be237a5f286e0aee523d33edea36c34baa9f
Daniel <daniel.song929@gmail.com>
2023-11-25 12:59:23 +1100

    EF1 alloc

---
+++ src/pages/tools/compsci/AI/resourceAlloc/EF1.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SectionBox } from '../../../../../components/UI/DefaultLayout'
+
+export default function EF1() {
+  return (
+    <SectionBox title="Algorithm for EF1 fairness">
+
+    </SectionBox>
+  )
+}
